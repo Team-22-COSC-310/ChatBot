a51d0c3fef015e41e9b20c0bf759a923fa09b16d
Varrz <ronan_mckenna@yahoo.com>
2022-02-24 16:21:21 -0800

    updated find_category

--- Code/category.py
+++ Code/category.py
@@ -1,20 +1,26 @@
 """
-This class is responsible for categorically identifying a sentence as either satisfaction (0), complaint (1), or review (2)
+This class is responsible for categorically identifying a sentence as either satisfaction, complaint, review, greeting, general, or closing
 """
 
 # feel free to add / remove words
-satisfactionWords = ["satisfied", "happy", "good", "love", "great", "amazing", "decent", "mediocre", "fun", "worked"]
+greetingWords = ["hi", "hello", "hey"]
+closingWords = ["bye", "goodbye", "farewell"]
+satisfactionWords = ["satisfied", "satisfying", "happy", "good", "love", "great", "amazing", "decent", "mediocre", "fun", "worked"]
 complaintWords = ["complaint", "complaints", "issue", "issues", "problem", "problems", "fail", "fails", "failed", "disappointing", "disappointed", "disappointment", "refund", "horrible", "boring", "bad", "irritating", "confusing", "confused", "annoying", "annoyed"]
 reviewWords = ["review", "rating", "feedback", "suggestion", "stars", "reviewed", "criticism", "suggestions"]
 
-def find_category(wordList: list) -> int:
+def find_category(wordList: list) -> str:
     """
     Iterates through every word in wordList and increments category score if the word is found in the category's list of keywords
     :param wordList: list
-    :return: int
+    :return: str
     """
-    satisfactionScore = 0; complaintScore = 0; reviewScore = 0
+    greetingScore = 0; closingScore = 0; satisfactionScore = 0; complaintScore = 0; reviewScore = 0
     for i, word in enumerate(wordList):
+        if word in greetingWords:
+            greetingScore += 1
+        if word in closingWords:
+            closingScore += 1
         if word in satisfactionWords:
             if wordList[i-1] == "not":
                 complaintScore += 1
@@ -27,10 +33,15 @@ def find_category(wordList: list) -> int:
                 complaintScore += 1
         if word in reviewWords:
             reviewScore += 1
-    
-    if satisfactionScore > complaintScore and satisfactionScore > reviewScore:
-        return 0    # satisfaction
-    elif complaintScore >= satisfactionScore and complaintScore >= reviewScore:
-        return 1    # complaint
-    elif reviewScore >= satisfactionScore and reviewScore > complaintScore:
-        return 2    # review
+
+    d = {}
+    d["greeting"] = greetingScore
+    d["closing"] = closingScore
+    d["product satisfaction"] = satisfactionScore
+    d["complaint"] = complaintScore
+    d["review"] = reviewScore
+
+    if sum(d.values()) > 0:
+        return max(d, key=d.get)
+    else:
+        return "general"
